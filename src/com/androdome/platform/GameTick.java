613a68024cff8b702a623fbed57971cc7dbba2d8
andreja6 <andrejaandjic@hotmail.com>
2018-05-14 13:05:28 -0700

    I cant think of any way to improve it

--- src/com/androdome/platform/GameTick.java
+++ src/com/androdome/platform/GameTick.java
@@ -35,7 +35,7 @@ public class GameTick extends Thread{
 					frame.player.onGround = false;
 					double scalesize = frame.gamepanel.getHeight()/GamePanel.scalefactor;
 					Point playerLocation = frame.gamepanel.getLevelRelativeLocation((int)((frame.player.location.x+frame.level.relativePoint.x+16)*scalesize), (int)((frame.player.location.y+frame.level.relativePoint.y+33)*scalesize));
-					if(!frame.gamepanel.collides(playerLocation.x, playerLocation.y))
+					if(!frame.gamepanel.collides(playerLocation.x, playerLocation.y) && !frame.gamepanel.collides(playerLocation.x-1, playerLocation.y))
 					{
 						dropTick++;
 						if(dropTick == 8)
@@ -62,21 +62,39 @@ public class GameTick extends Thread{
 						int left = Math.min(newLocationX.x, playerLocation.x);
 						int right = Math.max(newLocationX.x, playerLocation.x);
 						Point locy = null;
+						Point locy2 = null;
 						Point locx = null;
+						Point locyheadhit = null;
+						Point locyheadhit2 = null;
+						
 						if(frame.player.velocity.y < 0)
 						{
 							locy = frame.gamepanel.hitY(newLocationY.x, top, bottom-1);
+							locy2 = frame.gamepanel.hitY(newLocationY.x-1, top, bottom-1);
+							locyheadhit = frame.gamepanel.hitY(newLocationY.x, top-2, bottom-1);
+							locyheadhit2 = frame.gamepanel.hitY(newLocationY.x-1, top-2, bottom-1);
 						}
 						else
 						{
+							locy2 = frame.gamepanel.hitY(newLocationY.x-1, top, bottom);
 							locy = frame.gamepanel.hitY(newLocationY.x, top, bottom);
 						}
+						
+						if(locy == null && locy2 != null)
+							locy = locy2;
+						if(locyheadhit == null && locyheadhit2 != null)
+							locyheadhit = locyheadhit2;
+						
 						if(frame.player.velocity.x != 0)
 						{
-								locx = frame.gamepanel.hitX(newLocationY.y-2, left, right);		
+							if(locy != null)
+								locx = frame.gamepanel.hitX(newLocationY.y-1, left, right);		
+							else
+								locx = frame.gamepanel.hitX(newLocationY.y, left, right);
 						}
+						
 							
-						if(locy == null && locx == null)
+						if(locy == null && locx == null && locyheadhit == null)
 						{
 							frame.player.location.x+=frame.player.velocity.x;
 							frame.player.location.y+=frame.player.velocity.y;
@@ -86,11 +104,12 @@ public class GameTick extends Thread{
 						{
 							frame.player.location.x+=frame.player.velocity.x;
 							frame.player.velocity.y = 0;
+							if(locyheadhit == null)
 							frame.player.location.y = (int)((locy.y*16))-32;
 							frame.player.onGround = true;
 							dropTick = 0;
 						}
-						else if(locy == null)
+						else if(locy == null && locyheadhit == null)
 						{
 							frame.player.location.y+=frame.player.velocity.y;
 							if(frame.player.velocity.x > 0)
@@ -105,6 +124,7 @@ public class GameTick extends Thread{
 								frame.player.location.x = (int)((locx.x*16))-16;
 							else
 								frame.player.location.x = (int)((locx.x*16))+16;
+							if(locyheadhit == null)
 							frame.player.location.y = (int)((locy.y*16))-32;
 							frame.player.velocity.x = 0;
 							frame.player.velocity.y = 0;
